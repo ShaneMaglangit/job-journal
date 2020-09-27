931a2abac17d1d647e085cd2cfccac40211ac26c
ShaneMaglangit <shanedmaglangit@gmail.com>
2020-09-27 21:45:43 +0800

    Added layout for bottom sheet dialog from creating new records.

---
+++ app/src/main/java/com/shanemaglangit/jobjournal/newrecord/NewRecordDialogFragment.kt
@@ -0,0 +1,21 @@
+package com.shanemaglangit.jobjournal.newrecord
+
+import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.shanemaglangit.jobjournal.R
+
+class NewRecordDialogFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_new_record_dialog_list_dialog, container, false)
+    }
+}
