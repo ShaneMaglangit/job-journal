10e3b67ad766efb0d5e17b52214d20aace1f0683
ShaneMaglangit <shanedmaglangit@gmail.com>
2020-09-27 16:55:36 +0800

    Added navigation components

--- app/src/main/java/com/shanemaglangit/jobjournal/MainActivity.kt
+++ app/src/main/java/com/shanemaglangit/jobjournal/MainActivity.kt
@@ -2,10 +2,22 @@ package com.shanemaglangit.jobjournal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.shanemaglangit.jobjournal.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Inflate the layout and create the view binding
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Attach the navigation controller to the botton navigation view
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        NavigationUI.setupWithNavController(binding.bottomNav, navHost.navController)
     }
 }
