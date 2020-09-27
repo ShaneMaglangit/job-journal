d66907debca2d0a3fce6b0d61165189aa6a3aabd
ShaneMaglangit <shanedmaglangit@gmail.com>
2020-09-27 16:55:43 +0800

    Added record list fragment and view model

---
+++ app/src/main/java/com/shanemaglangit/jobjournal/recordlist/RecordListFragment.kt
@@ -0,0 +1,31 @@
+package com.shanemaglangit.jobjournal.recordlist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import com.shanemaglangit.jobjournal.R
+import com.shanemaglangit.jobjournal.databinding.FragmentRecordListBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RecordListFragment : Fragment() {
+    private lateinit var binding: FragmentRecordListBinding
+    private val viewModel : RecordListViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout and create the binding
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_record_list, container, false)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        return binding.root
+    }
+}
