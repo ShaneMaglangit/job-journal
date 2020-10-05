9896672ce43b005850f3d966b3f37eff20d5aad8
ShaneMaglangit <shanedmaglangit@gmail.com>
2020-10-06 00:11:00 +0800

    Override dispatchTouchEvent to hide the soft keyboard when edittext loses focus

--- app/src/main/java/com/shanemaglangit/jobjournal/MainActivity.kt
+++ app/src/main/java/com/shanemaglangit/jobjournal/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.shanemaglangit.jobjournal
 
+import android.app.Activity
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.format.DateUtils
+import android.view.MotionEvent
+import android.view.View
+import android.widget.EditText
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.shanemaglangit.jobjournal.data.AppDatabase
@@ -29,5 +34,30 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+    }
+
+
+    /**
+     * Hides the soft keyboard whenever a touch event is detected
+     * outside of edit text views.
+     */
+    override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+        if (ev.action == MotionEvent.ACTION_DOWN) {
+            val view = currentFocus
+            if (view != null && view is EditText) {
+                val r = Rect()
+                view.getGlobalVisibleRect(r)
+                val rawX = ev.rawX.toInt()
+                val rawY = ev.rawY.toInt()
+                if (!r.contains(rawX, rawY)) {
+                    val viewFocus = if (currentFocus == null) View(this) else currentFocus
+                    val inputMethodManager =
+                        getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+                    inputMethodManager.hideSoftInputFromWindow(viewFocus?.windowToken, 0)
+                    view.clearFocus()
+                }
+            }
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
